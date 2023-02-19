906494a9d920218af2f66b169d2bdfefd48ee3ea
Ozbridge <rishabhjain1909@gmail.com>
2023-02-19 15:45:36 +0530

    textstyle.dart

---
+++ lib/components/textstyle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../utils.dart';
+
+CustomTextStyle() {
+  return SafeGoogleFont(
+    'Montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Color(0xffffffff),
+  );
+}
