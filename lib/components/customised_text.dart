8573c9424096a6bd7c789d7a76dac9374b6d4fc7
Ojassvi Kumar <kumarojassvi@gmail.com>
2023-03-21 19:14:48 +0530

    Updated student home page UI

--- lib/components/customised_text.dart
+++ lib/components/customised_text.dart
@@ -3,19 +3,32 @@ import 'package:flutter/material.dart';
 
 class CustomisedText extends StatelessWidget {
   // ignore: prefer_typing_uninitialized_variables
-  final text, fontSize, color;
+  final text, fontSize, color, selectable;
 
-  const CustomisedText({
-    super.key,
-    required this.text,
-    this.fontSize = 20,
-    this.color = Colors.white,
-  });
+  const CustomisedText(
+      {super.key,
+      required this.text,
+      this.fontSize = 20,
+      this.color = Colors.white,
+      this.selectable = true});
 
   @override
   Widget build(BuildContext context) {
-    return SelectionArea(
-      child: Text(
+    if (selectable) {
+      return SelectionArea(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: SafeGoogleFont(
+            'Ubuntu',
+            fontSize: fontSize,
+            fontWeight: FontWeight.w700,
+            color: color,
+          ),
+        ),
+      );
+    } else {
+      return Text(
         text,
         textAlign: TextAlign.center,
         style: SafeGoogleFont(
@@ -24,7 +37,7 @@ class CustomisedText extends StatelessWidget {
           fontWeight: FontWeight.w700,
           color: color,
         ),
-      ),
-    );
+      );
+    }
   }
 }
