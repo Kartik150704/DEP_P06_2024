36863f499be43eeceafcfe669773f7c53916c786
Ozbridge <rishabhjain1909@gmail.com>
2023-04-04 20:31:12 +0530

    add offering form data validation

--- lib/components/add_project_form.dart
+++ lib/components/add_project_form.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:casper/components/text_field.dart';
 import 'package:casper/components/button.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:multiselect/multiselect.dart';
 
 class AddProjectForm extends StatefulWidget {
@@ -39,8 +40,9 @@ class _AddProjectFormState extends State<AddProjectForm> {
             ),
           ),
           FormBuilderTextField(
-            name: 'title',
-          ),
+              name: 'title',
+              validator: FormBuilderValidators.required(
+                  errorText: 'Title cannot be blank')),
           const SizedBox(
             height: 10,
           ),
@@ -55,6 +57,8 @@ class _AddProjectFormState extends State<AddProjectForm> {
           ),
           FormBuilderTextField(
             name: 'description',
+            validator: FormBuilderValidators.required(
+                errorText: 'Description cannot be blank'),
           ),
           const SizedBox(
             height: 10,
@@ -70,6 +74,17 @@ class _AddProjectFormState extends State<AddProjectForm> {
           ),
           FormBuilderTextField(
             name: 'semester',
+            validator: (value) {
+              if (value == null) {
+                return 'Semester cannot be blank';
+              } else if (int.tryParse(value) == null) {
+                return 'Semester must be numeric';
+              } else if (int.tryParse(value)! > 2 || int.tryParse(value)! < 1) {
+                return 'Enter valid semester';
+              } else {
+                return null;
+              }
+            },
           ),
           const SizedBox(
             height: 10,
@@ -85,6 +100,17 @@ class _AddProjectFormState extends State<AddProjectForm> {
           ),
           FormBuilderTextField(
             name: 'year',
+            validator: (value) {
+              if (value == null) {
+                return 'Year cannot be blank';
+              } else if (int.tryParse(value) == null) {
+                return 'Year must be numeric';
+              } else if (int.tryParse(value)! < 2000) {
+                return 'Enter valid year';
+              } else {
+                return null;
+              }
+            },
           ),
           const SizedBox(
             height: 10,
@@ -106,6 +132,8 @@ class _AddProjectFormState extends State<AddProjectForm> {
                       value: e,
                     ))
                 .toList(),
+            validator: FormBuilderValidators.required(
+                errorText: 'Type cannot be blank'),
           ),
           const SizedBox(
             height: 10,
@@ -139,29 +167,36 @@ class _AddProjectFormState extends State<AddProjectForm> {
                 buttonText: 'Submit',
                 onPressed: () {
                   _formKey.currentState?.save();
-                  final data = _formKey.currentState?.value.entries;
-                  var alldata = <String, dynamic>{};
-                  alldata.addEntries(data!);
-                  alldata.addEntries([MapEntry('open_for', vals)]);
-                  FirebaseFirestore.instance
-                      .collection('instructors')
-                      .where('uid',
-                          isEqualTo: FirebaseAuth.instance.currentUser?.uid)
-                      .get()
-                      .then((value) {
-                    var doc = value.docs[0];
-                    var entries = [
-                      ['department', doc['department']],
-                      ['instructor_name', doc['name']],
-                      ['status', 'open'],
-                    ];
-                    alldata
-                        .addEntries(entries.map((e) => MapEntry(e[0], e[1])));
-                    print(alldata);
-                    FirebaseFirestore.instance
-                        .collection('offerings')
-                        .add(alldata);
-                  });
+                  if (_formKey.currentState!.validate()) {
+                    if (vals.isEmpty) {
+                      return;
+                    } else {
+                      final data = _formKey.currentState?.value.entries;
+                      var alldata = <String, dynamic>{};
+                      alldata.addEntries(data!);
+                      alldata.addEntries([MapEntry('open_for', vals)]);
+                      FirebaseFirestore.instance
+                          .collection('instructors')
+                          .where('uid',
+                              isEqualTo: FirebaseAuth.instance.currentUser?.uid)
+                          .get()
+                          .then((value) {
+                        var doc = value.docs[0];
+                        var entries = [
+                          ['department', doc['department']],
+                          ['instructor_name', doc['name']],
+                          ['status', 'open'],
+                        ];
+                        alldata.addEntries(
+                            entries.map((e) => MapEntry(e[0], e[1])));
+                        print(alldata);
+                        FirebaseFirestore.instance
+                            .collection('offerings')
+                            .add(alldata);
+                        Navigator.pop(context);
+                      });
+                    }
+                  }
                 },
               ),
               CustomButton(
